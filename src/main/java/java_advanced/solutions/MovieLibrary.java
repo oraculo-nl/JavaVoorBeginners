c58daa76c8b61f661c686031724a214d0257da55
William Chau <w.chau1@student.han.nl>
2025-10-06 23:56:37 +0200

    oefening maken lambdas en streams WIP

--- src/main/java/java_advanced/solutions/MovieLibrary.java
+++ src/main/java/java_advanced/solutions/MovieLibrary.java
@@ -1,8 +1,6 @@
 package java_advanced.solutions;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 public class MovieLibrary {
 
@@ -13,7 +11,7 @@ public class MovieLibrary {
     }
 
     public void addAll(Collection<Movie> movies) {
-
+        this.movies.addAll(movies);
     }
 
     public List<Movie> all() {
@@ -24,11 +22,62 @@ public class MovieLibrary {
         return new ArrayList<>();
     }
 
-    public List<String> byGenre(String genre) {
-        return new ArrayList<>();
+
+    public List<Movie> byGenre(String genre) {
+        return this.movies.stream()
+                .filter(m -> m.genre()
+                        .equals(genre))
+                .toList();
     }
 
     public boolean anyHighRated(double threshold) {
+        return false;
+    }
 
+    public double averageRating() {
+        return 0;
+    }
+
+    public List<Movie> topNLongest(int n) {
+        return new ArrayList<>();
+    }
+
+    public List<String> uniqueGenres() {
+        return movies.stream()
+                .map(Movie::genre)
+                .distinct()
+                .sorted()
+                .toList();
+    }
+
+    public Map<String, Long> countByGenre() {
+        return new HashMap<>();
+    }
+
+    public Map<String, Movie> bestPerGenre() {
+        var genres = uniqueGenres();
+        Map<String, Movie> bestPerGenre = new HashMap<>();
+        genres.forEach(g -> this.movies.stream()
+                .filter(m -> m.genre()
+                        .equalsIgnoreCase(g))
+                .max(Comparator.comparing(Movie::rating))
+                .ifPresent(m -> bestPerGenre.put(g, m)));
+        return bestPerGenre;
+    }
+
+    public List<String> titlesByYearRange(int startYear, int endYear) {
+        return new ArrayList<>();
+    }
+
+    public int totalWatchTimeOf(String genre) {
+        return 0;
+    }
+
+    public List<String> recommendations(double minRating, int maxLength) {
+        return new ArrayList<>();
+    }
+
+    public List<String> searchTokensToTitles(List<List<String>> tokens) {
+        return new ArrayList<>();
     }
 }
---
+++ src/main/java/java_advanced/solutions/Test.java
@@ -0,0 +1,8 @@
+package java_advanced.solutions;
+
+public class Test {
+    public static void main(String[] args) {
+        var m = new Movie("deer hunter", "drama", 8.5, 120, 2019);
+        System.out.println(m.genre());
+    }
+}
